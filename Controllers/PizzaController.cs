2497ff9bde7f5263f8daac3bd2a20cb6e6303f1d
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-11-22 14:17:24 +0100

    create/update pizza pages and controllers

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -7,22 +7,66 @@ namespace la_mia_pizzeria.Controllers
 {
     public class PizzaController : Controller
     {
+        PizzaDbContext db;
+
+        public PizzaController() : base()
+        {
+            db = new PizzaDbContext();
+        }
         public IActionResult Index()
         {
-            PizzaDbContext db = new PizzaDbContext();
-
+       
            List<Pizza> pizzaList = db.Pizzas.ToList();
 
             return View(pizzaList);
         }
         public IActionResult Details(int id)
         {
-
-            PizzaDbContext db = new PizzaDbContext();
-
             Pizza singlePizza = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
 
             return View(singlePizza);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza newPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            db.Pizzas.Add(newPizza);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Update(int id)
+        {
+            Pizza pizzaToSearch = db.Pizzas.Where(pizzaToSearch => pizzaToSearch.Id == id).FirstOrDefault();
+
+            if (pizzaToSearch == null)
+                return NotFound();
+
+            //return View() --> non funziona perchè non ha la memoria della post
+            return View(pizzaToSearch);
+        }
+        [HttpPost]
+        public IActionResult Update(Pizza pizzaToUpdate)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            db.Pizzas.Update(pizzaToUpdate);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
